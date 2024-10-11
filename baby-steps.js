aba76f6934bdffaa0e89be502e18372649ad651b
Borhan Thais <borhanthais@gmail.com>
2024-10-11 16:12:16 +0600

    Modified

--- baby-steps.js
+++ baby-steps.js
@@ -1,5 +1,3 @@
-let sum=0;
-for(let i=2;i<process.argv.length;i++){
-sum+= +(process.argv[i]);
+for (let sum = 0, i = 2; i < process.argv.length; i++) {
+  sum += +process.argv[i];
 }
-console.log(sum);
